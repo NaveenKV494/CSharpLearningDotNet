1cf8a87a91ebe7c31b35338553a09f2ad88f0443
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-15 19:13:49 +0530

    Add project files.

---
+++ CSharpLearningDotNet/DragonBall.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+    var DB1 = new DragonBall ( "Goku", "UltraInstinct","Son",32 );
+    var DB2 = new DragonBall("Vegeta", "SuperSaiyanGod", "Vegeta", 29);
+//DB1.Names();
+//DB2.Names();
+
+
+    DB1.Moves.Add(new Finish("KameHameHa"));
+    DB1.Moves.Add(new Attack("InstantHit"));
+
+    DB2.Moves.Add(new Finish("FinalFlash"));
+    DB2.Moves.Add(new Attack("FlashBeam"));
+
+List<DragonBall> DBCharacters = [DB1,DB2];
+foreach (var DBC in DBCharacters)
+{
+    Console.WriteLine( $"{DBC}" );
+    foreach (var move in DBC.Moves)
+    {
+        Console.WriteLine($"      {move}"); 
+    }
+}
+
+    Console.WriteLine(DBCharacters.Count);
+
+    public class DragonBall(string DragonBallname, string Saiyanlevel, string Family, int wins)
+    {
+
+        public string DBname { get; } = DragonBallname;
+
+        public string Slevel { get; } = Saiyanlevel;
+
+        public string Family { get; } = Family;
+
+        public int wins { get; } = wins;
+
+        public List<Move> Moves { get;} = new ();
+
+    }
+
+//public void Names()
+//{
+//    Console.WriteLine(DBname);
+//}
+public abstract class Move (string name)
+    {
+        public string Name { get; } = name;
+        public abstract string Action();
+        public override string ToString()
+        {
+            return $"The {GetType().Name} of  is {name}";
+        }
+    }
+
+    public class  Finish (string name) : Move (name)
+    {
+        public override string Action() => "Kills";
+
+    }
+
+    public class Attack (string name) : Move (name)
+    {
+        public override string Action() => "Attacks";
+    }
+
+
+
+
---
+++ CSharpLearningDotNet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearningDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            var names = new List<string> { "Naveen", "Shanmugam","Jana"};
+            names.Add("Selva");
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}"); 
+            }
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[3]);
+
+            var cars = new string[] { "Mustang", "Challenger", "Camaro SS" };
+            cars = [..cars, "Thunderbird"];
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"My favourite car is {car}"); 
+            }
+
+            var numbers = new List<int> { 45, 46, 99, 89, 23, 11, 4 };
+            Console.WriteLine($"The index of 99 is {numbers.IndexOf(99)}");
+            numbers.Sort();
+            
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number}");
+            }
+            Console.WriteLine($"The index of 99 is {numbers.IndexOf(99)}");
+        }
+    }
+}
+
